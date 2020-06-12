cf65e2de0e9c799dc49291a02ab887f0f78c5fe5
weitang li <liwt31@163.com>
2020-06-12 13:03:10 +0800

    update doc

--- renormalizer/utils/tests/test_basis.py
+++ renormalizer/utils/tests/test_basis.py
@@ -8,20 +8,18 @@ import pytest
 def test_BasisSHO(op, x0):
 
     sho = Ba.BasisSHO(0.1, 10, x0=x0, dvr=False)
-    sho_general = Ba.BasisSHO(0.1, 10, x0=x0, dvr=False)
-    sho_general.general_xp_power = True
+    sho_general = Ba.BasisSHO(0.1, 10, x0=x0, general_xp_power=True, dvr=False)
 
     a = sho.op_mat(op)
     b = sho_general.op_mat(op)
-    assert np.allclose(a,b)
+    assert np.allclose(a, b)
 
     sho_dvr = Ba.BasisSHO(0.1, 10, x0=x0, dvr=True)
-    sho_dvr_general = Ba.BasisSHO(0.1, 10, x0=x0, dvr=True)
-    sho_dvr_general.general_xp_power = True
+    sho_dvr_general = Ba.BasisSHO(0.1, 10, x0=x0, general_xp_power=True, dvr=True)
     a_dvr = sho_dvr.op_mat(op)
     b_dvr = sho_dvr_general.op_mat(op)
     a_dvr = sho_dvr.dvr_v  @ a_dvr @ sho_dvr.dvr_v.T
-    b_dvr = sho_dvr.dvr_v  @ b_dvr @ sho_dvr.dvr_v.T
+    b_dvr = sho_dvr_general.dvr_v  @ b_dvr @ sho_dvr_general.dvr_v.T
     if op == "x^2":
         # the last basis is not accurate in dvr
         assert np.allclose(a[:-1,:-1], a_dvr[:-1,:-1]) 
