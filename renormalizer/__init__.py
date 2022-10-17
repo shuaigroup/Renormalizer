6007bf3f6caf8e4ae694d6741f7bd1a69829376c
Weitang Li <liwt31@163.com>
2022-10-17 09:16:32 +0800

    logging env variable and fancy theme (#140)
    Co-authored-by: weitang li <weitangli@tencent.com>

--- renormalizer/__init__.py
+++ renormalizer/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Author: Jiajun Ren <jiajunren0522@gmail.com>
+import logging
 import os
 import sys
 import warnings
@@ -17,13 +18,17 @@ if reno_num_threads is not None:
             os.environ[env] = reno_num_threads
             del env
 
-del os, sys, warnings
+del sys, warnings
 
 from renormalizer.utils.log import init_log
 
-init_log()
 
-del init_log
+# logging.DEBUG == 10, logging.INFO == 20. See the logging built-in package for more
+log_level = int(os.environ.get("RENO_LOG_LEVEL", logging.DEBUG))
+
+init_log(log_level)
+
+del os, log_level, init_log
 
 
 # user interfaces
