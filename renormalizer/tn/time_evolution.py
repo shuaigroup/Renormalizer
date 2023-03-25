439ba89c3a3c8c39857d3e25843ce134d898f98e
liwt31 <liwt31@163.com>
2023-03-25 20:34:15 +0800

    add vmf

---
+++ renormalizer/tn/time_evolution.py
@@ -0,0 +1,90 @@
+from typing import Union
+import logging
+
+import scipy
+import opt_einsum as oe
+
+from renormalizer.mps.backend import np
+from renormalizer.lib import solve_ivp
+from renormalizer.tn.tree import TensorTreeOperator, TensorTreeState, TensorTreeEnviron
+from renormalizer.tn.hop_expr import hop_expr1
+
+
+logger = logging.getLogger(__name__)
+
+
+def time_derivative_vmf(tts: TensorTreeState, tto: TensorTreeOperator):
+    # first get it right. Then add QN. Then benchmark and optimize
+    environ_s = TensorTreeEnviron(tts, TensorTreeOperator.identity(tts.basis))
+    environ_h = TensorTreeEnviron(tts, tto)
+
+    deriv_list = []
+    for inode, node in enumerate(tts.node_list):
+        hop, _ = hop_expr1(node, tts, tto, environ_h)
+        # idx1: children+physical, idx2: parent
+        dim_parent = node.shape[-1]
+        shape_2d = (-1, dim_parent)
+        deriv = hop(node.tensor).reshape(shape_2d)
+        if node.parent is None:
+            deriv_list.append(deriv.ravel())
+            continue
+        tensor = node.tensor.reshape(shape_2d)
+        proj = tensor.conj() @ tensor.T
+        ovlp = environ_s.node_list[inode].environ_parent.reshape(dim_parent, dim_parent)
+        ovlp_inv = regularized_inversion(ovlp, 1e-8)
+        deriv = oe.contract("bf, bg, fh -> gh", deriv, np.eye(proj.shape[0]) - proj, ovlp_inv.T)
+        deriv_list.append(deriv.ravel())
+    return np.concatenate(deriv_list)
+
+
+def regularized_inversion(m, eps):
+    # XXX: this is debug code
+    evals, evecs = scipy.linalg.eigh(m)
+    evals_i, evecs_i = scipy.linalg.eigh(m + np.eye(len(m)) * eps)
+    scipy.linalg.inv(m + np.eye(len(m)) * eps)
+    weight1 = 1
+    weight2 = np.exp(-evals / eps)
+    weight3 = np.random.rand(len(evals)) * 2
+    evals1 = evals + eps * weight1
+    # np.testing.assert_allclose(evals_i, evals1)
+    evals2 = evals + eps * weight2
+    evals3 = evals + eps * weight3
+    new_evals = 1 / evals1
+    # print(new_evals)
+    return evecs @ np.diag(new_evals) @ evecs.T
+
+
+def regularized_inversion(m, eps):
+    m = m + np.eye(len(m)) * eps
+    return scipy.linalg.pinv(m)
+
+
+def regularized_inversion2(m, eps):
+    evals, evecs = scipy.linalg.eigh(m)
+    weight = np.exp(-evals / eps)
+    evals = evals + eps * weight
+    m = evecs @ np.diag(evals) @ evecs.T
+    return scipy.linalg.pinv(m)
+
+
+def evolve(tts:TensorTreeState, tto:TensorTreeOperator, tau:Union[complex, float], first_step=None):
+    imag_time = np.iscomplex(tau)
+    # trick to avoid complex algebra
+    # exp{coeff * H * tau}
+    # coef and tau are different from MPS implementation
+    if imag_time:
+        coef = 1
+        tau = tau.imag
+    else:
+        coef = -1j
+        tts = tts.to_complex()
+
+    def ivp_func(t, params):
+        tts_t = TensorTreeState.from_tensors(tts, params)
+        return coef * time_derivative_vmf(tts_t, tto)
+    init_y = np.concatenate([node.tensor.ravel() for node in tts.node_list])
+    sol = solve_ivp(ivp_func, (0, tau), init_y, first_step=first_step)
+    logger.info(f"VMF func called: {sol.nfev}. RKF steps: {len(sol.t)}")
+    new_tts = TensorTreeState.from_tensors(tts, sol.y[:, -1])
+    new_tts.canonicalise()
+    return new_tts
